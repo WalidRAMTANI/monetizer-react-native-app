b1ee099911284fc1e0cbf7d385ac618011147a41
Votre Nom <votre.email@example.com>
2026-04-17 16:24:57 +0200

    feat: peoject setup, routing, tab navigation

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+export default function RootLayout() {
+  return (
+    <SafeAreaView>
+      <Stack
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+      </Stack>
+    </SafeAreaView>
+  );
+}
