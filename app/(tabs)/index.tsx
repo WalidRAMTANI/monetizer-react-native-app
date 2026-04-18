c5b3e503cfffdbff82dd82e2cf5d1be22c5291d7
Votre Nom <votre.email@example.com>
2026-04-18 03:41:18 +0200

    feat: impliment home UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,90 @@
-import { Link } from "expo-router";
-import { Text } from "react-native";
+import ListHeader from "@/components/ListHeader";
+import SubscriptionCard from "@/components/SubscriptionCard";
+import UpCommingSubscriptionCard from "@/components/UpCommingSubscriptionCard";
+import {
+  HOME_BALANCE,
+  HOME_SUBSCRIPTIONS,
+  HOME_USER,
+  UPCOMING_SUBSCRIPTIONS,
+} from "@/constants/data";
+import { icons } from "@/constants/icons";
+import images from "@/constants/images";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
+import { useState } from "react";
+import { FlatList, Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 export default function App() {
+  const [expandedId, setExpandedId] = useState<string | null>(null);
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-      <Link href="/onboarding">
-        <Text className="text-blue-500 underline">Go to onboarding</Text>
-      </Link>
-      <Link href="/sign-in">
-        <Text className="text-blue-500 underline">Go to Sign In</Text>
-      </Link>
-      <Link href="/sign-up">
-        <Text className="text-blue-500 underline">Go to Sign Up</Text>
-      </Link>
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <View className="flex-1">
+        <FlatList
+          contentContainerClassName="pb-20"
+          ListHeaderComponent={() => (
+            <>
+              <View className="home-header">
+                <View className="home-user">
+                  <Image source={images.avatar} className="home-avatar" />
+                  <Text className="home-user-name">{HOME_USER.name}</Text>
+                </View>
+                <Image source={icons.add} className="home-add-icon" />
+              </View>
+
+              <View className="home-balance-card">
+                <Text className="home-balance-label">Balance</Text>
+                <View className="home-balance-row">
+                  <Text className="home-balance-amount">
+                    {formatCurrency(HOME_BALANCE.amount)}
+                  </Text>
+                  <Text className="home-balance-date">
+                    {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}
+                  </Text>
+                </View>
+              </View>
+
+              <View>
+                <ListHeader title="Upcoming" />
+                <FlatList
+                  data={UPCOMING_SUBSCRIPTIONS}
+                  renderItem={({ item }) => (
+                    <UpCommingSubscriptionCard {...item} />
+                  )}
+                  keyExtractor={(item) => item.id}
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  ListEmptyComponent={
+                    <Text className="home-empty-state">
+                      {" "}
+                      No Upcoming renewals yet.
+                    </Text>
+                  }
+                />
+              </View>
+              <ListHeader title="All Subscriptions" />
+            </>
+          )}
+          data={HOME_SUBSCRIPTIONS}
+          renderItem={({ item }) => (
+            <SubscriptionCard
+              {...item}
+              expanded={expandedId === item.id}
+              onPress={() => {
+                setExpandedId(item.id === expandedId ? null : item.id);
+              }}
+            />
+          )}
+          extraData={expandedId}
+          keyExtractor={(item) => item.id}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={
+            <Text className="home-empty-state">
+              No Upcoming Subscriptions yet.
+            </Text>
+          }
+          ItemSeparatorComponent={() => <View className="h-4" />}
+        />
+      </View>
     </SafeAreaView>
   );
 }
---
+++ constants/images.ts
@@ -0,0 +1,7 @@
+import avatar from "@/assets/images/avatar.png";
+import splashPattern from "@/assets/images/splash-pattern.png";
+const images = {
+  avatar,
+  splashPattern,
+};
+export default images;
